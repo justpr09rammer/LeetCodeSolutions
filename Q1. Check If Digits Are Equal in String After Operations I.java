5be3cdc3d94bba8ef2bcdc500acec41f65348ec8
nihad.001 <163444756+justpr09rammer@users.noreply.github.com>
2025-02-23 06:14:41 -0800

    Create Q1. Check If Digits Are Equal in String After Operations I.java

---
+++ Q1. Check If Digits Are Equal in String After Operations I.java
@@ -0,0 +1,26 @@
+
+class Solution {
+    public boolean hasSameDigits(String s) {
+        int n = s.length();
+        if (n == 1 || n == 0){
+            return false;
+        }
+        while (n > 2){
+            int leftPointer = 0;
+            StringBuilder temp = new StringBuilder();
+            //System.out.println(s);
+            while (leftPointer < n - 1){
+                Integer a = Integer.parseInt(String.valueOf(s.charAt(leftPointer)));
+                Integer b = Integer.parseInt(String.valueOf(s.charAt(leftPointer + 1)));
+                int c = (a + b) % 10;
+                temp.append(Integer.toString(c));
+                leftPointer ++;
+            }
+
+            n = temp.length();
+            s = temp.toString();
+        }
+        //System.out.println(s);
+        return s.charAt(0) == s.charAt(1);
+    }
+}
