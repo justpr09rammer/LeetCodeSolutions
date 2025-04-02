20d2c459dd4ba6414c9a48d669b55ed76fe93071
nihad.001 <163444756+justpr09rammer@users.noreply.github.com>
2025-04-01 22:32:50 -0700

    brute force

---
+++ 2873. Maximum Value of an Ordered Triplet I.java
@@ -0,0 +1,15 @@
+
+class Solution {
+    public long maximumTripletValue(int[] nums) {
+        long maxValue = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    maxValue = Math.max(maxValue, (long)(nums[i] - nums[j]) * nums[k]);
+                }
+            }
+        }
+        return maxValue;
+    }
+}
