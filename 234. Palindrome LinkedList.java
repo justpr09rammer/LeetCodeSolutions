79d2059a36ba6fabcdd118ecbae828a4c82655d9
nihad.001 <163444756+justpr09rammer@users.noreply.github.com>
2025-04-14 09:18:02 -0700

    Create 234. Palindrome LinkedList.java
    Firstly what i did is bsaically finding the middle node of the linked list, before middle too, since I ll make beforeMiddle.next = null to have two distinct linked list, then I just reversed the second half , that's all. (checking too)

---
+++ 234. Palindrome LinkedList.java
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    //kinda global variable
+    private boolean isEvenLength = true;
+    // for the odd length linked lists
+    private ListNode beforeMiddle = null;
+    public boolean isPalindrome(ListNode head) {
+
+        if (head.next == null){
+            return true;
+        }
+
+
+        ListNode middle = findMiddleNode(head);
+        ListNode head2;
+        beforeMiddle.next = null;
+        //for odd length
+        if (!isEvenLength){
+            head2 = middle.next;
+        }
+        //for even length
+        else {
+            head2 = middle;
+        }
+        head2 = reverse(head2);
+        while (head!= null && head2 != null){
+            if (head.val != head2.val){
+                return false;
+            }
+            head = head.next;
+            head2 = head2.next;
+        }
+        return true;
+
+    }
+    private ListNode findMiddleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            beforeMiddle = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (fast != null) {
+            isEvenLength = false;
+        }
+        return slow;
+    }
+    private ListNode reverse(ListNode head) {
+        ListNode previous = null;
+        ListNode current = head;
+        ListNode next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
+    }
+
+
+}
