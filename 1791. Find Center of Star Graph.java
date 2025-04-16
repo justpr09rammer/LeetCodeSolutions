0fc381fdc72ed2b493e593c20d3880e7797fdad5
nihad.001 <163444756+justpr09rammer@users.noreply.github.com>
2025-04-16 14:26:58 -0700

    brute force

---
+++ 1791. Find Center of Star Graph.java
@@ -0,0 +1,15 @@
+
+class Solution {
+    public int findCenter(int[][] edges) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int[] edge : edges) {
+            map.put(edge[0], map.getOrDefault(edge[0], 0) + 1);
+            map.put(edge[1], map.getOrDefault(edge[1], 0) + 1);
+        }
+        int n = edges.length;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == n) return entry.getKey();
+        }
+        return -1;
+    }
+}
