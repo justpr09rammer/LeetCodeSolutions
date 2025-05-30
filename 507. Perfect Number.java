d61113459958db59f1e3891326179c63c3f3dc1f
nihad.001 <163444756+justpr09rammer@users.noreply.github.com>
2025-05-30 12:42:25 -0700

    optimal solution

---
+++ 507. Perfect Number.java
@@ -0,0 +1,18 @@
+
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int res = 0;
+        for (int i = 1; i <= Math.sqrt(num); i ++){
+            if (num % i == 0){
+                if (i != num / i){
+                    //System.out.println(i);
+                    res += i;
+                    res += (num / i);
+                    //System.out.println(num / i);
+                }
+            }
+        }
+        res -= num;
+        return res == num;
+    }
+}
