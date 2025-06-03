682c674b3e0192e266285ace217644c88e99e77d
nihad.001 <163444756+justpr09rammer@users.noreply.github.com>
2025-06-03 13:01:03 -0700

    Update 507. Perfect Number.java

--- 507. Perfect Number.java
+++ 507. Perfect Number.java
@@ -5,10 +5,14 @@ class Solution {
         for (int i = 1; i <= Math.sqrt(num); i ++){
             if (num % i == 0){
                 if (i != num / i){
-                    //System.out.println(i);
+                    System.out.println(i);
                     res += i;
                     res += (num / i);
-                    //System.out.println(num / i);
+                    System.out.println(num / i);
+                }
+                else{
+                    System.out.println(i);
+                    res += i;
                 }
             }
         }
